ce0e9e1ebad347ca9ce1cd3ff7d248e862039740
KillianMalon <killian.malon@epsi.fr>
2023-04-03 22:21:20 +0200

    Change Name App

--- flight_school_booking/store/apps.py
+++ flight_school_booking/store/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FlightSchoolConfig(AppConfig):
+class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'flight_school'
+    name = 'store'
